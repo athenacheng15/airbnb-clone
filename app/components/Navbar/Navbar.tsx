85cf881b72bd5dd49012a4e93d11db62e3ceef9d
Athena Cheng <athenachengyc15@gmail.com>
2023-06-04 22:45:26 +0800

    feat(@components/Navbar) : UserMenu and Avatar layout

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Container from "../Container";
 
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
 	return (
@@ -13,6 +14,7 @@ const Navbar = () => {
 					<div className="flex flex-row items-center justify-between gap-3 md:gap-0">
 						<Logo />
 						<Search />
+						<UserMenu />
 					</div>
 				</Container>
 			</div>
