a30ecac5fd89c31c6d0515e6044cf683017da904
Athena Cheng <athenachengyc15@gmail.com>
2023-06-11 02:53:19 +0800

    feat(@components/ClientOnly) : add ClientOnly component to prevent hydration error

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps {
+	children: React.ReactNode;
+}
+
+export default ({ children }: ClientOnlyProps) => {
+	const [hasMounted, setHasMounted] = useState<boolean>(false);
+
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
+
+	if (!hasMounted) return null;
+
+	return <>{children}</>;
+};
