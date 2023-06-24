331f1d41de071a664b44ad9c2d2b787a62e2a2d9
Athena Cheng <athenachengyc15@gmail.com>
2023-06-25 01:00:23 +0800

    feat(@components/Button) : modify hover

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -28,7 +28,7 @@ export default ({
 			className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full
         ${
 					outline
-						? "bg-white border-black text-black"
+						? "bg-white border-black text-black hover:bg-neutral-100"
 						: "bg-rose-500 border-rose-500 text-white"
 				}
         ${
