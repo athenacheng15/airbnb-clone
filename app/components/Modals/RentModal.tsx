3838f035d2821420c059f5f4889690bacbd825aa
Athena Cheng <athenachengyc15@gmail.com>
2023-08-20 15:41:15 +0800

    feat(@Components/Modal):add country select logic to rentModal

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import useRentModal from "@/app/hooks/useRentModal";
-import Modal from "./Modal";
 import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+import useRentModal from "@/app/hooks/useRentModal";
+
+import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../Navbar/constant";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
 	CATEGORY = 0,
@@ -44,6 +47,7 @@ export default () => {
 	});
 
 	const category = watch("category");
+	const location = watch("location");
 
 	const setCustomValue = (id: string, value: any) => {
 		setValue(id, value, {
@@ -65,7 +69,7 @@ export default () => {
 		if (step === STEPS.PRICE) {
 			return "Create";
 		}
-		return "Back";
+		return "Next";
 	}, [step]);
 
 	const secondaryActionLabel = useMemo(() => {
@@ -96,11 +100,26 @@ export default () => {
 		</div>
 	);
 
+	if (step === STEPS.LOCATION) {
+		bodyContent = (
+			<div className="flex flex-col gap-8">
+				<Heading
+					title="Where is your place located?"
+					subtitle="Help guests find you!"
+				/>
+				<CountrySelect
+					value={location}
+					onChange={(value) => setCustomValue("location", value)}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={rentModal.onClose}
+			onSubmit={onNext}
 			actionLabel={actionLabel}
 			secondaryActionLabel={secondaryActionLabel}
 			secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
