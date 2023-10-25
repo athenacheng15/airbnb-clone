7fcf29d8a5aa0ab1f86a3ac31a1e779eafb5ef0c
yuchuncheng <yuchuncheng@nine-yi.com>
2023-10-25 18:14:28 +0800

    feat(@Component/listings):create ListingReservation component

---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Range } from 'react-date-range';
+
+interface ListingReservationProps {
+    price: number;
+    totalPrice: number;
+    onChangeDate: (value: Range) => void;
+    dateRange: Range;
+    onSubmit: () => void;
+    disabled: boolean;
+    disabledDates: Date[];
+}
+
+export default ({
+    price,
+    totalPrice,
+    onChangeDate,
+    dateRange,
+    onSubmit,
+    disabled,
+    disabledDates,
+}: ListingReservationProps) => {
+    return (
+        <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
+            <div className="flex flex-row items-center gap-1 p-4">
+                <div className="text-2xl font-semibold">$ {price}</div>
+                <div className="font-light text-neutral-600">night</div>
+            </div>
+            <hr />
+        </div>
+    );
+};
